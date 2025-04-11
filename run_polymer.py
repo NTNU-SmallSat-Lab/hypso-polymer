143e29bb4b5b6c79d37bc6d88f2d869ff475adec
Cameron P <cameron.l.penne@ntnu.no>
2025-04-11 15:00:19 +0200

    Add run_polymer.py

---
+++ run_polymer.py
@@ -0,0 +1,88 @@
+import numpy as np
+from importlib.resources import files
+from urllib.parse import urlparse, unquote
+
+from pathlib import Path
+import netCDF4 as nc
+
+import sys
+import os
+
+from hypso import Hypso
+from hypso.write import write_l1c_nc_file
+
+POLYMER_PATH = '/home/cameron/Projects/polymer/'
+
+def main(l1a_nc_path, lats_path=None, lons_path=None) -> np.ndarray:
+
+    # Check if the first file exists
+    if not os.path.isfile(l1a_nc_path):
+        print(f"Error: The file '{l1a_nc_path}' does not exist.")
+        return
+
+    # Process the first file
+    print(f"Processing file: {l1a_nc_path}")
+
+    l1a_nc_path = Path(l1a_nc_path)
+
+    satobj = Hypso(path=l1a_nc_path, verbose=True)
+
+    # Run indirect georeferencing
+    if lats_path is not None and lons_path is not None:
+        try:
+
+            with open(lats_path, mode='rb') as file:
+                file_content = file.read()
+            
+            lats = np.frombuffer(file_content, dtype=np.float32)
+
+            lats = lats.reshape(satobj.spatial_dimensions)
+
+            with open(lons_path, mode='rb') as file:
+                file_content = file.read()
+            
+            lons = np.frombuffer(file_content, dtype=np.float32)
+  
+            lons = lons.reshape(satobj.spatial_dimensions)
+
+
+            # Directly provide the indirect lat/lons loaded from the file. This function will run the track geometry computations.
+            satobj.run_indirect_georeferencing(latitudes=lats, longitudes=lons)
+
+        except Exception as ex:
+            print(ex)
+            print('Indirect georeferencing has failed. Defaulting to direct georeferencing.')
+
+            satobj.run_direct_georeferencing()
+
+    else:
+        satobj.run_direct_georeferencing()
+
+    satobj.generate_l1b_cube()
+    satobj.generate_l1c_cube()
+
+    write_l1c_nc_file(satobj, overwrite=True, datacube=False)
+
+    l1c_nc_path = satobj.l1c_nc_file
+
+    output_path = satobj.parent_dir
+
+    #del satobj
+    #satobj=None
+
+
+    print("Polymer Done!")
+
+ 
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2 or len(sys.argv) > 4:
+        print("Usage: python run_acolite.py <l1a_nc_path> [lats_path] [lons_path]")
+        sys.exit(1)
+
+    l1a_nc_path = sys.argv[1]
+    
+    lats_path = sys.argv[2] if len(sys.argv) == 4 else None
+    lons_path = sys.argv[3] if len(sys.argv) == 4 else None
+
+    main(l1a_nc_path, lats_path, lons_path)
